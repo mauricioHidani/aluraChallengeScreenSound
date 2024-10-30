b50f17f3af1fc8f66a43fef46a2ca06a34ece877
MauricioHidani <mauricio.hidani.work@gmail.com>
2024-10-30 15:03:47 -0300

    feat: NotFoundException

---
+++ src/main/java/br/com/challenge/alura/sound/screen/exceptions/NotFoundException.java
@@ -0,0 +1,7 @@
+package br.com.challenge.alura.sound.screen.exceptions;
+
+public class NotFoundException extends RuntimeException {
+	public NotFoundException(String message) {
+		super(message);
+	}
+}
